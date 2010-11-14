1f90aace662a10161ecf7edf7acfa33d116f8692
farmer1992@gmail.com <farmer1992@gmail.com@a78df781-2512-ce25-8a3b-c3ff3943b039>
2010-11-14 10:48:11 +0000

    init version

---
+++ taobb.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+import string,cgi,time
+from os import curdir, sep
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+from urlparse import parse_qs
+from urllib import unquote 
+
+from urlkeys import base62_encode as tokey
+from urlkeys import base62_decode as fromkey
+from urluni import url_uni
+from hash import hash
+import urldb
+
+html = open('taobb.html').read()
+
+class MyHandler(BaseHTTPRequestHandler):
+	
+	def err(self):
+		self.send_response(302)
+		self.send_header('Location','/')
+		self.end_headers()
+		
+	def do_HEAD(self):
+		self.do_GET()
+
+	def do_GET(self):
+		try:
+			path = self.path.lstrip(' /')
+			url = ''
+			
+			path = path.split('?')
+			err = ''
+
+			if len(path[0]) == 5:
+				url = urldb.fromdb(fromkey(path[0]))
+				if url:
+					self.send_response(302)
+					self.send_header('Location',url)
+					self.end_headers()
+					return
+
+			elif path[0] !='':
+				self.err()		
+			elif len(path) > 1 :
+				query = parse_qs(path[1])
+				if query.has_key('url'):
+					url = unquote(query['url'][0])
+					url = url_uni(url)
+					if url:
+						urldb.todb(hash(url), url)
+						key = tokey(hash(url))
+					else:
+						err = '非法的URL'	
+			else:
+				pass
+
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			if not key:
+				url = ''
+
+			if len(url) == 5:
+				url = "http://tao.bb/" + url
+
+			self.wfile.write(html)
+			return
+		except Exception:
+			self.err()		
+
+
+
+def main():
+	try:
+		server = HTTPServer(('', 8008), MyHandler)
+		print 'ok...'
+		server.serve_forever()
+	except KeyboardInterrupt:
+		server.socket.close()
+
+if __name__ == '__main__':
+	main()
+
---
+++ urldb.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+conn = sqlite3.connect('url.db')
+c = conn.cursor()
+
+def fromdb(i):
+	c.execute("select `url` from `urls` where id = %d" % i)
+	url = c.fetchone()
+	if url:
+		url = url[0]
+	return url
+
+def todb(i, url):
+	c.execute("""replace into `urls` values (%d,'%s')""" % (i, url))
+	conn.commit()
