2d77e0f465e3bc457439df0fd8e15108beceeb3c
farmer1992@gmail.com <farmer1992@gmail.com@a78df781-2512-ce25-8a3b-c3ff3943b039>
2010-11-25 12:40:53 +0000

    学习 google sb url 整理

--- trunk/urluni.py
+++ trunk/urluni.py
@@ -1,35 +1,73 @@
 #!/usr/bin/env python
 
+import string
+
 from urlparse import urlsplit
 from urlparse import parse_qsl
 from urllib import urlencode
 from urllib import quote
+from urllib import unquote
 import re
 
-httpre = re.compile("https?:\/\/", re.IGNORECASE)
+
+"""
+inspired a lot from 
+http://google-safe-browsing.googlecode.com/svn/trunk/python/expression.py
+"""
+
+SAFE_CHARS = ''.join([c for c in (string.digits + string.ascii_letters + string.punctuation) if c not in '%#'])
+
+def escape(unescaped_str):
+	unquoted = unquote(unescaped_str)
+	while unquoted != unescaped_str:
+		unescaped_str = unquoted
+		unquoted = unquote(unquoted)
+
+	return quote(unquoted, SAFE_CHARS)
+
+def popath(path):
+	path = path or '/'
+	path = path if path[0] != '/' else '/' + path
+	path = escape(path)
+	
+	path_components = []
+	for path_component in path.split('/'):
+		if path_component == '..':
+			if len(path_components) > 0:
+				path_components.pop()
+		elif path_component != '.' and path_component != '':
+			path_components.append(path_component)
+	
+	canonical_path = '/' + '/'.join(path_components)
+	if path.endswith('/') and not canonical_path.endswith('/'):
+		canonical_path += '/'
+	return canonical_path
+	
 
 def url_uni(url):
-	url = url.strip()
-	if not httpre.match(url):
-		if url.find('://') > 0:
-			return None
-		else:
-			url = 'http://' + url
-		
-	url = urlsplit(url)
+	url = url.replace('\t', '').replace('\r', '').replace('\n', '')
+	#url = escape(url.strip())
+	testurl = urlsplit(url)
+	if not testurl.scheme in ['http','https']:
+		url = urlsplit('http://' + url)
+	else:
+		url = testurl
 
 	scheme = url.scheme
-
-	netloc = url.netloc
-	if netloc:
-
+	if url.netloc:
 		try:
 			port = url.port
 			username = url.username
 			password = url.password
-			hostname = url.hostname.decode('utf-8').encode('idna')
+
+			hostname = [part for part in url.hostname.split('.') if part]
+			if len(hostname) < 2:
+				return None
+			hostname = '.'.join(hostname)
+			hostname = hostname.decode('utf-8').encode('idna').lower()
 		except:
 			return None
+
 
 		netloc = hostname
 		if username or password:
@@ -45,7 +83,7 @@ def url_uni(url):
 				port = '' if port == 443 else port
 			netloc += ':' + str(port)
 		
-		path = netloc.lower() + re.sub('/+','/',url.path)
+		path = netloc + popath(url.path)
 	else:
 		return None
 
@@ -56,4 +94,4 @@ def url_uni(url):
 
 	fragment = url.fragment
 
-	return (('%s://%s?%s#%s' % (scheme, quote(path), query, quote(fragment))).rstrip('?#/ '))
+	return (('%s://%s?%s#%s' % (scheme, escape(path), query, escape(fragment))).rstrip('?#/ '))
